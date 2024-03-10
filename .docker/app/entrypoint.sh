3e3c6aad0c7afdb1c78d317ed6af17396fc52ccb
André Santos <andre.forweb@gmail.com>
2024-03-10 19:42:01 -0300

    Ajuste imagens e testes

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -2,12 +2,20 @@
 
 vendorFolder=/app/vendor
 
-if ! [ -f "$vendorFolder" ]; then
+if [ ! -d "$vendorFolder" ]; then
+    mkdir /app/vendor
     printf "\n- Composer install \n"
-    composer install && composer dumpautoload
+    cd /app && composer install && composer dump-autoload
     chown 1000:1000 -R /app/vendor
+    touch /app/composer-installed && chown 1000:1000 /app/composer-installed
 else
-  printf "\n- Composer já instalado, seguindo... \n"
+    printf "\n- Diretório vendor criado, seguindo... \n"
 fi
+
+while [ ! -f "/app/composer-installed" ]
+do
+  printf "\n- Aguardando instalação das dependências do composer... \n"
+  sleep 2s
+done
 
 /usr/local/bin/rr serve -c /app/.rr.yaml
